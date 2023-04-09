ea797abe71d17ff3a06703095618f17f15b364d4
Vladimir Voblikov <vladimir.voblikov.774@gmail.com>
2023-04-10 02:57:37 +0500

    Git init ex 2.2.2 (2)

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,5 +1,6 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +14,11 @@ import java.util.List;
 @Controller
 public class CarController {
 
-    private final CarService carService = new CarServiceImp();
+    private final CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
 
     @GetMapping("/cars")
     public String getCars(@RequestParam(value = "count", defaultValue = "5") Integer count, Model model) {
--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -1,20 +1,25 @@
 package web.service;
 
+import org.springframework.stereotype.Controller;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Controller
 public class CarServiceImp implements CarService {
 
-    private final List<Car> cars = Arrays.asList(
+    private List<Car> cars;
+
+    {
+        cars = Arrays.asList(
             new Car("Lada1", "1", "Blue1"),
             new Car("Lada2", "2", "Blue2"),
             new Car("Lada3", "3", "Blue3"),
             new Car("Lada4", "4", "Blue4"),
-            new Car("Lada5", "5", "Blue5")
-    );
+            new Car("Lada5", "5", "Blue5"));
+    }
 
 
     @Override
