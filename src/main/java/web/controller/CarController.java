a1ae6f9bc03d1b692ce671dfbaad9938b0145c11
Vladimir Voblikov <vladimir.voblikov.774@gmail.com>
2023-04-10 03:06:52 +0500

    Git init ex 2.2.2 (3)

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,14 +1,10 @@
 package web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.service.CarService;
-import web.service.CarServiceImp;
-import java.util.List;
 
 
 @Controller
@@ -22,8 +18,7 @@ public class CarController {
 
     @GetMapping("/cars")
     public String getCars(@RequestParam(value = "count", defaultValue = "5") Integer count, Model model) {
-        List<Car> cars = carService.getCars(count);
-        model.addAttribute("cars", cars);
+        model.addAttribute("cars", carService.getCars(count));
         return "cars";
     }
 }
